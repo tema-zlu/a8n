9e8cfd6a036f8477cff20a96d1cec64d68336cfb
tema-zlu <tema.zlu@gmail.com>
2026-04-15 22:34:44 +0300

    ci: add base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: process.env.CI ? '/a8n/' : undefined,
   resolve: {
     tsconfigPaths: true,
   },
